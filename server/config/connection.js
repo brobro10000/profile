9653048e3dfc1f8b54c390bc253b81e6e75e2991
Hamzah <82611798+brobro10000@users.noreply.github.com>
2022-02-11 21:31:20 -0500

    delete

--- server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ecommerce", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
